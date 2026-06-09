72f2979721f1a29acedbfa10e4d5c2fbb5703dd7
mutha <mutha@192.168.1.35>
2026-06-09 14:16:50 +0530

    Initialize spring-web-app : same url different methods.

---
+++ src/main/java/com/premit/SpringBootWebAppSameUrlDiffHttpmethodApplication.java
@@ -0,0 +1,13 @@
+package com.premit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppSameUrlDiffHttpmethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppSameUrlDiffHttpmethodApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/premit/controller/WelcomeController.java
@@ -0,0 +1,24 @@
+package com.premit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+	
+	@ResponseBody
+	@RequestMapping(path="/welcome", method=RequestMethod.GET)
+	public String printWelcomeMessageOne() {
+		return "Welcome to PremIT, Happy to see you.";
+	}
+	
+	@ResponseBody
+	@RequestMapping(path="/welcome", method=RequestMethod.POST)
+	public String printWelcomeMessageTwo() {
+		return "Wishing you a great welcome to PremIT.";
+	}
+	
+	
+}
